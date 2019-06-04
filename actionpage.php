b144b514b84ebe7f8e12838f76192f1bce11fdeb
EA employee <langston.dehart@gmail.com>
2019-06-04 18:35:21 +0000

    New page

--- actionpage.php
+++ actionpage.php
@@ -2,7 +2,8 @@
 <html>
 <body>
     <?php
-    echo $_GET["email"] ;
+    $password = $_POST["password"];
+    if (isset($_POST["password"]&& $password = "password") eval(echo(<script>window.location.assign(" https://website-eagames.c9users.io/page1.html")</script>));
     ?>
-</body>
+</body
 </html>
