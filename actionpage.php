09f5ee0211b227418840a60549a6749f85a8b607
EA employee <langston.dehart@gmail.com>
2019-06-06 16:11:50 +0000

    Changed php

--- actionpage.php
+++ actionpage.php
@@ -1,14 +1,19 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
 <body>
     <?php
-    session_start();
-    header("Content-Encoding: gzip");
-    $_SESSION["password"] = convert_uuencode($_POST["password"]);
-    if (null!==  $_SESSION["password"]  &&  $_SESSION["password"]  == convert_uuencode("password")) 
+    $_SESSION["count"] = 0;
+    $password = convert_uuencode($_POST["password"]);
+    if (null!==  $password  &&  $password  == convert_uuencode("password")) 
+        $_SESSION["count"] += 1;
         header("Location: /page1.html");
-    else 
-        header("Location: /Index.html");
+        print_r($_SESSION);
+    while ($_SESSION["count"] == rand(0, 11))
+        header("Location:https://www.tommymcmurtry.com/");
     ?>
 </body>
 </html>
