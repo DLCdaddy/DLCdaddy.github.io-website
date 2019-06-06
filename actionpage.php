bd77b77d81914de6b34377cbe2ecbe8ce5614fec
EA employee <langston.dehart@gmail.com>
2019-06-06 16:36:50 +0000

    php changes

--- actionpage.php
+++ actionpage.php
@@ -1,18 +1,16 @@
-<?php
-session_start();
-?>
+
 
 <!DOCTYPE html>
 <html>
 <body>
     <?php
-    $_SESSION["count"] = 0;
+    $number = rand(0, 11);
     $password = convert_uuencode($_POST["password"]);
     if (null!==  $password  &&  $password  == convert_uuencode("password")) 
         $_SESSION["count"] += 1;
+          setcookie("test",$_SESSION);
         header("Location: /page1.html");
-        print_r($_SESSION);
-    while ($_SESSION["count"] == rand(0, 11))
+    while ($number == rand(0, 11))
         header("Location:https://www.tommymcmurtry.com/");
     ?>
 </body>
