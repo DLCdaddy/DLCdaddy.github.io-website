79e1ed34e43f10dfb8e017a5e07a0759ed4d8035
EA employee <langston.dehart@gmail.com>
2019-06-06 20:36:29 +0000

    secret feature added

--- actionpage.php
+++ actionpage.php
@@ -1,17 +1,22 @@
-
+<?php
+session_start();
+$_SESSION["count"]=0;
+?>
 
 <!DOCTYPE html>
 <html>
 <body>
     <?php
     $number = rand(0, 11);
+    setcookie("cookie",$number);
     $password = convert_uuencode($_POST["password"]);
     if (null!==  $password  &&  $password  == convert_uuencode("password")) 
         $_SESSION["count"] += 1;
-          setcookie("test",$_SESSION);
+          setcookie("cookie",$_SESSION);
         header("Location: /page1.html");
-    while ($number == rand(0, 11))
+    if ($number == rand(0, 11))
         header("Location:https://www.tommymcmurtry.com/");
+        $number = 0;
     ?>
 </body>
 </html>
