f2166fb2c10e64749ff0719eedfa0648007c7773
EA employee <langston.dehart@gmail.com>
2019-06-07 14:55:26 +0000

    php changes

--- actionpage.php
+++ actionpage.php
@@ -5,65 +5,31 @@
     <?php
     $number = rand(0, 11);
     $other_number = rand(0,11);
-    $website = rand(1,10);
+    $website = rand(1,2);
     setcookie("cookie",$number);
     setcookie("other_cookie",$other_number);
     setcookie("website", $website);
     $password = convert_uuencode($_POST["password"]);
-    if (null!==  $password  &&  $password  == convert_uuencode("password")) 
+    if (null!==  $password  &&  $password  == convert_uuencode("password") && $number != $other_number) 
         $_SESSION["count"] += 1;
           setcookie("cookie",$_SESSION);
           header("Location: /page1.html");
-    if ($number == $other_number && $website == 1)
-            header("Location:http://corndog.io/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
+    if($number == $other_number && $website == 1)
+        setcookie("cookie",$number);
+        setcookie("other_cookie",$other_number);
+        setcookie("website", $website);
+        $number = rand(0, 11);
+        $other_number = rand(0,11);
+        $website = rand(1,10);
+        header("Location:http://corndog.io/");
     if ($number == $other_number && $website = 2)
-            header("Location:http://tinytuba.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 3)
-            header("Location:http://www.movenowthinklater.com");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 4)
-            header("Location:https://heeeeeeeey.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 5)
-            header("Location:https://www.eyes-only.net/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 6)
-            header("Location:http://burymewithmymoney.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 7)
-            header("Location:http://burymewithmymoney.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 8)
-            header("Location:http://beesbeesbees.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 9)
-            header("Location:https://cat-bounce.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
-    if ($number == $other_number && $website = 10)
-            header("Location:http://ninjaflex.com/");
-            $number = rand(0, 11);
-            $other_number = rand(0,11);
-            $website = rand(1,10);
+        $number = rand(0, 11);
+        $other_number = rand(0,11);
+        $website = rand(1,10);
+        setcookie("cookie",$number);
+        setcookie("other_cookie",$other_number);
+        setcookie("website", $website);
+        header("Location:http://tinytuba.com/");
     ?>
 </body>
 </html>
