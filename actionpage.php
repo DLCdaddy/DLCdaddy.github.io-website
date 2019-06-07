d179a3ecc6d354889dae4ab84ddeb3eb411c060c
EA employee <langston.dehart@gmail.com>
2019-06-07 18:58:44 +0000

    final commit

--- actionpage.php
+++ actionpage.php
@@ -14,8 +14,12 @@
         header("Location:http://corndog.io/");
     if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 2)
         header("Location:http://tinytuba.com/");
-     if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 3)
-     header("Location:https://trypap.com/")
+    if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 3)
+     header("Location:https://trypap.com/");
+    if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 4)
+     header("http://www.partridgegetslucky.com/");
+    if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 5)
+     header("http://www.hackertyper.com/");
     ?>
 </body>
 </html>
