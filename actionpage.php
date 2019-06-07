f43e4823eaec16c03152f9881394e986353603d7
EA employee <langston.dehart@gmail.com>
2019-06-07 15:31:46 +0000

    changes

--- actionpage.php
+++ actionpage.php
@@ -10,11 +10,11 @@
     setcookie("other_cookie",$other_number);
     setcookie("website", $website);
     $password = convert_uuencode($_POST["password"]);
-    if(null !==  $password  &&  $password  == convert_uuencode("password") && $number != $other_number) 
+    if(null !==  $password  &&  $password == convert_uuencode("password") && $number != $other_number) 
         header("Location: /page1.html");
-    if(null !==  $password  &&  $password  == convert_uuencode("password") && $number == $other_number && $website == 1)
+    if(null !==  $password  &&  $password == convert_uuencode("password") && $number == 10 && $website == 1)
         header("Location:http://corndog.io/");
-    if(null !==  $password  &&  $password  == convert_uuencode("password") && $number == $other_number && $website == 2)
+    if(null !== $password && $password  == convert_uuencode("password") && $number == 10 && $website == 2)
         $number = rand(0, 10);
         $other_number = rand(0,10);
         $website = rand(1,2);
