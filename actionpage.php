20b2f269d9b8b8cb026e200ff494bf1679b2964e
EA employee <langston.dehart@gmail.com>
2019-06-07 15:51:02 +0000

    Random website redirector finally works!

--- actionpage.php
+++ actionpage.php
@@ -3,17 +3,19 @@
 <html>
 <body>
     <?php
-    $number = rand(0, 10);
-    $website = rand(1,2);
+    $number = rand(0, 5);
+    $website = rand(1,3);
     setcookie("cookie" ,$number);
     setcookie("website", $website);
     $password = convert_uuencode($_POST["password"]);
-    if(null !==  $password  &&  $password == convert_uuencode("password") && $number != 10) 
+    if(null !==  $password  &&  $password == convert_uuencode("password") && $number != 5) 
         header("Location: /page1.html");
-    if(null !==  $password  &&  $password == convert_uuencode("password") && $number == 10 && $website == 1)
+    if(null !==  $password  &&  $password == convert_uuencode("password") && $number == 5 && $website == 1)
         header("Location:http://corndog.io/");
-    if(null !== $password && $password  == convert_uuencode("password") && $number == 10 && $website == 2)
+    if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 2)
         header("Location:http://tinytuba.com/");
+     if(null !== $password && $password  == convert_uuencode("password") && $number == 5 && $website == 3)
+     header("Location:https://trypap.com/")
     ?>
 </body>
 </html>
