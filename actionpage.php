e7d0513abd39967819d48b3f1631beb4b4eae2a0
EA employee <langston.dehart@gmail.com>
2019-06-07 15:35:57 +0000

    changes

--- actionpage.php
+++ actionpage.php
@@ -4,20 +4,15 @@
 <body>
     <?php
     $number = rand(0, 10);
-    $other_number = rand(0,10);
     $website = rand(1,2);
     setcookie("cookie" ,$number);
-    setcookie("other_cookie",$other_number);
     setcookie("website", $website);
     $password = convert_uuencode($_POST["password"]);
-    if(null !==  $password  &&  $password == convert_uuencode("password") && $number != $other_number) 
+    if(null !==  $password  &&  $password == convert_uuencode("password") && $number != 10) 
         header("Location: /page1.html");
     if(null !==  $password  &&  $password == convert_uuencode("password") && $number == 10 && $website == 1)
         header("Location:http://corndog.io/");
     if(null !== $password && $password  == convert_uuencode("password") && $number == 10 && $website == 2)
-        $number = rand(0, 10);
-        $other_number = rand(0,10);
-        $website = rand(1,2);
         header("Location:http://tinytuba.com/");
     ?>
 </body>
